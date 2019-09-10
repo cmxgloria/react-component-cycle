43efc79bba24b76aade6c98d2979ac097f8cc95d
Gloria Chen <cmxgloria@gmail.com>
2019-09-10 22:31:43 +1000

    added constructor

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './counter';
-import CSS from './counter';
+import CSS from './app.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mount: true
+    }
+    this.mount = () => this.setState({ mount: true })
+    this.unmount = () => this.setState({ mount: false })
+  }
+  render() {
+    return <div>
+      <Counter />
+    </div>
+  }
+}
+ReactDOM.render(<App />, getElementById('root'));
