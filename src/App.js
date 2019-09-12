c1deb2bdd3d8937e3ee99a0949ea554b6f7ac531
Gloria Chen <cmxgloria@gmail.com>
2019-09-12 13:29:40 +1000

    added buttons

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ class App extends React.Component {
   }
   render() {
     return <div>
+      <button onClick={this.mountCounter}>Mount Counter</button>
+      <button onClick={this.unmountCounter}>Unmount Counter</button>
       <Counter />
     </div>
   }
