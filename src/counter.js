c72d48a18df3eae053e1bee6efc058ca32d23ad1
Gloria Chen <cmxgloria@gmail.com>
2019-09-12 13:11:03 +1000

    added increment decrement

---
+++ src/counter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+    this.increment = () => this.setState({ counter: this.state.counter + 1 });
+    this.decrement = () => this.setState({ counter: this.state.counter - 1 });
+  }
+  render() {
+    return <div>
+      <div className="counter">
+        Counter:{this.state.counter}
+      </div>
+    </div>
+  }
+}
