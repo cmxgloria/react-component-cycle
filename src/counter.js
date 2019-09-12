5cd798cf95d48563fb1a9a97931198e9cd3f73fe
Gloria Chen <cmxgloria@gmail.com>
2019-09-12 13:19:53 +1000

    comp didmount updated

--- src/counter.js
+++ src/counter.js
@@ -9,6 +9,10 @@ export default class Counter extends React.Component {
     this.increment = () => this.setState({ counter: this.state.counter + 1 });
     this.decrement = () => this.setState({ counter: this.state.counter - 1 });
   }
+  componentDidMount() {
+    console.log("component did mount");
+    console.log("-------------------");
+  }
   render() {
     return <div>
       <button onClick={this.increment}>Increment</button>
@@ -17,5 +21,9 @@ export default class Counter extends React.Component {
         Counter:{this.state.counter}
       </div>
     </div>
+  }
+  componentDidUpdate(prevProps, prevstate, snapshot) {
+    console.log('component did update');
+    console.log("-----------")
   }
 }
